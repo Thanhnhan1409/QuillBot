7ce6944f3c5efb491c14994cdad9becee7786411
Thanhnhan1409 <phanthithanhnhan1409@gmail.com>
2024-05-09 00:14:08 +0700

    fix: remove el-plus lib

--- src/api/openai.ts
+++ src/api/openai.ts
@@ -1,4 +1,4 @@
-import { GoogleGenerativeAI } from '@google/generative-ai';
+import { GoogleGenerativeAI } from '@google/generative-ai'
 
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_APP_AI_KEY)
 
@@ -50,20 +50,22 @@ export const openaiApi = {
     },
     async getParaphraseTextFull(fullContent: string) {
         try {
-            const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
+            const model = genAI.getGenerativeModel({ model: 'gemini-pro' })
             const prompt = `You are a helpful paraphraser.
             You should return the matching language of the original text.
             You should not change the meaning of the text
             You must only reply to the paraphrased text without any extra comments or explanations.
+            Note: If the original language is English, respond in English; if it is Vietnamese, respond in Vietnamese, and similarly for other languages.
             For example: Mèo là loài rất dễ thương -> Mèo là loài rất đáng yêu
             For another example: Cats are very cute animals -> Cats are adorable animals
-            Now please paraphrase this: ${fullContent}`;
-            const result = await model.generateContent(prompt);
-            const response = await result.response;
-            const text = await response.text();
-            return text;
-        } catch {
+            Now please paraphrase this: ${fullContent}`
+            const result = await model.generateContent(prompt)
+            const response = await result.response
+            const text = await response.text()
+            return text
+        }
+        catch {
             return 'Hề hề'
         }
-    }
+    },
 }
