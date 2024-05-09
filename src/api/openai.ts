e7be460e6c19d3677ad50a42452272b2b34db79b
Thanhnhan1409 <phanthithanhnhan1409@gmail.com>
2024-05-09 20:03:04 +0700

    feat: replace selected words

--- src/api/openai.ts
+++ src/api/openai.ts
@@ -4,57 +4,42 @@ const genAI = new GoogleGenerativeAI(import.meta.env.VITE_APP_AI_KEY)
 
 export const openaiApi = {
 
-    async getParaphraseText(content: string, fullContent: string) {
-        try {
-            const response = await fetch('https://api.openai.com/v1/chat/completions', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${import.meta.env.VITE_APP_AI_KEY}`,
-                },
-                body: JSON.stringify({
-                    model: 'gpt-3.5-turbo',
-                    messages: [
-                        {
-                            role: 'system',
-                            content: `You are a helpful paraphraser.
-                        You should return the matching language of the original text.
-                        You should not change the meaning of the text
-                        You must only reply to the paraphrased text without any extra comments or explanations.
-                        For example
-                        Full text: Mèo là loài rất dễ thương
-                        Paraphrased text: rất dễ thương
-                        Result: rất đáng yêu
-                        For another example
-                        Full text: Cats are very cute animals
-                        Paraphrased text: cute animals
-                        Result: adorable animals
-                        Now please paraphrase this
-                        Full text: ${content}
-                        Paragraphed text: ${fullContent}`,
-                        },
-                        {
-                            role: 'user',
-                            content: 'Hello!',
-                        },
-                    ],
-                }),
-            })
-
-            const chatCompletion = await response.json()
-            return chatCompletion?.choices?.[0].message?.content || ''
-        }
-        catch {
-            return 'Hề hề'
-        }
-    },
-    async getParaphraseTextFull(fullContent: string) {
+    async getParaphraseText(content: string, fullContent: string, language: string) {
         try {
             const model = genAI.getGenerativeModel({ model: 'gemini-pro' })
             const prompt = `You are a helpful paraphraser.
             You should return the matching language of the original text.
             You should not change the meaning of the text
             You must only reply to the paraphrased text without any extra comments or explanations.
+            You should convert with the same language ${language} and replace words with similar expressions
+            For example
+            Full text: Mèo là loài rất dễ thương
+            Paraphrased text: rất dễ thương
+            Result: rất đáng yêu
+            For another example
+            Full text: Cats are very cute animals
+            Paraphrased text: cute animals
+            Result: adorable animals
+            Now please paraphrase this
+            Full text: ${content}
+            Paragraphed text: ${fullContent}`
+            const result = await model.generateContent(prompt)
+            const response = await result.response
+            const text = await response.text()
+            return text
+        }
+        catch {
+            return 'Hề hề'
+        }
+    },
+    async getParaphraseTextFull(fullContent: string, language: string) {
+        try {
+            const model = genAI.getGenerativeModel({ model: 'gemini-pro' })
+            const prompt = `You are a helpful paraphraser.
+            You should return the matching language of the original text.
+            You should not change the meaning of the text
+            You must only reply to the paraphrased text without any extra comments or explanations.
+            You should convert with the same language ${language} and replace words with similar expressions
             Note: If the original language is English, respond in English; if it is Vietnamese, respond in Vietnamese, and similarly for other languages.
             For example: Mèo là loài rất dễ thương -> Mèo là loài rất đáng yêu
             For another example: Cats are very cute animals -> Cats are adorable animals
